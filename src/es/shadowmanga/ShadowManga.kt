75ca3eec04f937b522fb3018dc4cb24ef436c5bd
cesarcsnvz <cesar.casanova.zu@gmail.com>
2026-04-27 00:54:06 -0500

    Delete src/es/shadowmanga/ShadowManga.kt

--- src/es/shadowmanga/ShadowManga.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.es.shadowmanga
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class ShadowManga : Madara(
-    "ShadowManga",
-    "https://shadowmanga.es",
-    "es"
-)
